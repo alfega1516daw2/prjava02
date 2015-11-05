aca2f27973f889ba45a6f6af416ef066831681da
alfega1516daw2 <alfega1516daw2@gmail.com>
2015-11-05 18:43:09 +0100

    Treballant amb ordres Git i branques v0.2

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -24,5 +24,6 @@ public class Prjava02
             } 
         catch (IOException e) {}
         System.out.println("Creació d'una branca del projecte prjava02");
+        System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         } 
     } 
