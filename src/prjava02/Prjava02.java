1cf606d191c47c816c2273d5d8bbd1dbe26a15f4
alfega1516daw2 <alfega1516daw2@gmail.com>
2015-11-07 17:50:33 +0100

    Creant un primer conflicte amb la branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -25,5 +25,6 @@ public class Prjava02
         catch (IOException e) {}
         System.out.println("Creació d'una branca del projecte prjava02");
         System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+        System.out.println("Conflicte típic: Codi diferent a una mateixa línia");
         } 
     } 
